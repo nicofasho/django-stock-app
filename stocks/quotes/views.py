78763420f6b3f0a875ab4c8f8b3d92174b855da1
Christian Watson <chris23w@gmail.com>
2022-02-01 15:08:26 -0800

    add stock form and delete

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Stock
+from .forms import StockForm
+from django.contrib import messages
 
 
 def home(request):
@@ -28,4 +31,20 @@ def about(request):
 
 
 def add_stock(request):
-    return render(request, 'add_stock.html', {})
+    if request.method == 'POST':
+        form = StockForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, ("Stock Has Been Added"))
+            return redirect('add_stock')
+    else:
+        ticker = Stock.objects.all()
+        return render(request, 'add_stock.html', {'ticker': ticker})
+
+
+def delete(request, stock_id):
+    item = Stock.objects.get(pk=stock_id)
+    item.delete()
+    messages.success(request, ("Stock Has Been Deleted!"))
+    return redirect(add_stock)
