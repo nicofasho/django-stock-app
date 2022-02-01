023c906b82a1a7201bafb8bcdb67fa6947a502d9
Christian Watson <chris23w@gmail.com>
2022-02-01 13:14:30 -0800

    initial api setup

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -2,7 +2,21 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+
+    # pk_c06698a456be48b3926f6f4d439a8141
+    api_request = requests.get(
+        'https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_c06698a456be48b3926f6f4d439a8141'
+    )
+
+    try:
+        api = json.loads(api_request.content)
+
+    except Exception as e:
+        api = 'Error...'
+
+    return render(request, 'home.html', {'api': api})
 
 
 def about(request):
